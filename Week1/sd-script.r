d5446e84dd831bab5486442fa617acb7b6e41564
Nathan van Beelen <nathan@vanbeelen.org>
2019-09-16 19:42:00 +0200

    Update standard deviation code

--- Week1/sd-script.r
+++ Week1/sd-script.r
@@ -79,20 +79,45 @@ vmvymkuiflxoctostaxvgstxrwpkcvkigytllmymmklizlauis
 ameroixvdxlbhtprptxbbbmlkfvxrqzxtguiipwitfrpcgzzrt
 ogstxaqcaix"
 
+sqr <- function(x) {
+  x * x
+}
+
 encryp <- gsub("[\r\n]", "", encrypted_text)
+encrypt_split <- strsplit(gsub("[\r\n]", "", encrypted_text), "")[[1]]
 
 for (i in 5:15) {
   total_sd <- 0
   current_position <- 0
   vector_length <- nchar(encryp) / i
-  for (j in 1:i) {
-    frequencies <- as.vector(table(strsplit(substring(encryp, current_position, current_position + vector_length), "")))
-    #print(substring(encryp, current_position, current_position + vector_length))
-    current_position <- current_position + vector_length + 1
-    #print(table(strsplit(substring(encryp, vector_length * (j - 1), vector_length * j), "")))
-    total_sd <- total_sd + sqrt( sum(frequencies * frequencies) / 26 - sqr( sum(frequencies) / 26 ))
-    #total_sd <- total_sd + sd(frequencies)
+  vectors <- c()
+  
+  # Iterate over all the characters and put them in the right vectors.
+  position = 1
+  for (character in encrypt_split) {
+    wrap_number <- (position %% i) + 1
+    if (is.null(vectors[wrap_number]) || is.na(vectors[wrap_number])) {
+      vectors[wrap_number] <- character
+    } else {
+      vectors[wrap_number] <- paste(vectors[wrap_number], character, sep="")
+    }
+    position <- position + 1
   }
+  #frequencies <- as.vector(table(strsplit(substring(encryp, current_position, current_position + vector_length), "")))
+  #print(substring(encryp, current_position, current_position + vector_length))
+  #current_position <- current_position + vector_length + 1
+  #print(table(strsplit(substring(encryp, vector_length * (j - 1), vector_length * j), "")))
+  #total_sd <- total_sd + sqrt( sum(frequencies * frequencies) / 26 - sqr( sum(frequencies) / 26 ))
+  #total_sd <- total_sd + sd(frequencies)
+  
+  # Calculate the sd per vector.
+  frequencies <- c()
+  for (k in 1:i) {
+    frequencies[k] <- as.vector(table(strsplit(vectors, "")[k]))
+    #print(as.vector(table(strsplit(vectors, "")[k])))
+  }
+  #total_sd <- sqrt( sum(frequencies * frequencies) / 26 - sqr( sum(frequencies) / 26 ))
+  total_sd <- sd(frequencies)
   #for (j in seq(from = i, to = nchar(encryp), by = i)) {
   #  frequencies <- as.vector(table(strsplit(substring(encryp, j - i, j), "")))
   #  total_sd <- total_sd + sd(frequencies)
