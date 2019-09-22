973bc652caf779c47dbb97f5f99a3c144f1a22b4
Nathan van Beelen <nathan@vanbeelen.org>
2019-09-22 22:00:01 +0200

    Comment the sd script

--- Week1/sd-script.r
+++ Week1/sd-script.r
@@ -86,6 +86,7 @@ sqr <- function(x) {
 encryp <- gsub("[\r\n]", "", encrypted_text)
 encrypt_split <- strsplit(gsub("[\r\n]", "", encrypted_text), "")[[1]]
 
+# For all possible key sizes, calculate the standard deviation.
 for (i in 5:15) {
   total_sd <- 0
   current_position <- 0
@@ -93,6 +94,8 @@ for (i in 5:15) {
   vectors <- c()
   
   # Iterate over all the characters and put them in the right vectors.
+  # These vectors are based on the (potential) character in a word.
+  # Assuming the key size is correct, you'll get a shifted alphabet in each vector.
   position = 1
   for (character in encrypt_split) {
     if (is.null(vectors[position]) || is.na(vectors[position])) {
@@ -106,57 +109,23 @@ for (i in 5:15) {
       position <- 1
     }
   }
-  #frequencies <- as.vector(table(strsplit(substring(encryp, current_position, current_position + vector_length), "")))
-  #print(substring(encryp, current_position, current_position + vector_length))
-  #current_position <- current_position + vector_length + 1
-  #print(table(strsplit(substring(encryp, vector_length * (j - 1), vector_length * j), "")))
-  #total_sd <- total_sd + sqrt( sum(frequencies * frequencies) / 26 - sqr( sum(frequencies) / 26 ))
-  #total_sd <- total_sd + sd(frequencies)
   
-  # Calculate the sd per vector.
-  #frequencies <- c()
+  # Calculate the sd per vector and sum them for all vectors.
   for (k in 1:i) {
-    #frequencies[k] <- as.vector(table(strsplit(vectors, "")[k]))
     frequencies <- as.vector(table(strsplit(vectors, "")[k]))
     total_sd <- total_sd + sqrt( sum(frequencies * frequencies) / 26 - sqr( sum(frequencies) / 26 ))
-    #print(as.vector(table(strsplit(vectors, "")[k])))
   }
-  #total_sd <- sqrt( sum(frequencies * frequencies) / 26 - sqr( sum(frequencies) / 26 ))
-  #total_sd <- sd(frequencies)
-  #for (j in seq(from = i, to = nchar(encryp), by = i)) {
-  #  frequencies <- as.vector(table(strsplit(substring(encryp, j - i, j), "")))
-  #  total_sd <- total_sd + sd(frequencies)
-  #}
+  # Print the standard deviation for the key.
   cat("Key", i, "sd:", total_sd, "\n")
-}
-
-# Compare frequencies.
-vectors <- vector()
-position <- 0
-for (character in encrypt_split) {
-  wrap_number <- (position %% 9) + 1
-  #print(wrap_number)
-  if (is.null(vectors[wrap_number]) || is.na(vectors[wrap_number])) {
-    vectors[wrap_number] <- character
-  } else {
-    vectors[wrap_number] <- paste(vectors[wrap_number], character, sep="")
-  }
-  if (position < 9) {
-    position <- position + 1
-  } else {
-    position <- 1
-  }
 }
 
 # List most likely key characters
 alphabet = c('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-three <- c('e', 'o', 'g')
+three <- c('e', 'o', 'g') # most frequent characters according to the assignment
 distances <- c(match(three[1], alphabet) - 1, match(three[2], alphabet) - 1, match(three[3], alphabet) - 1)
-characters <- matrix(nrow = 3, ncol = 9)
 for (k in 1:9) {
-  #cat(k)
-  #print(sort(table(strsplit(vectors[k], "")), decreasing=T))
   x <- vector()
+  # For all three possible letters, get the likely key character by shifting.
   for (l in 1:3) {
     letter <- rownames(sort(table(strsplit(vectors[k], "")), decreasing=T))[l]
     temp_distance <- match(letter, alphabet) - distances[l]
@@ -174,6 +143,9 @@ decrypted_string <- ""
 alphabet = c('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 keystring <- c('i', 'n', 't', 'e', 'g', 'r', 'i', 't', 'y')
 position <- 1
+
+# For every character we shift the character in the alphabet according to the respective distance.
+# This distance determines on the position in the keyword. This results in a decrypted text.
 for (character in encrypt_split) {
   distance <- match(character, alphabet) - match(keystring[position], alphabet)
   if (distance < 0) {
@@ -186,5 +158,6 @@ for (character in encrypt_split) {
     position <- 1
   }
 }
+
 print(decrypted_string)
 
