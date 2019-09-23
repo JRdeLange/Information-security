5d0940ee90725722378e4697326824f0a3956995
tux <mrshroom1988@gmail.com>
2019-09-23 23:30:19 +0200

    ex6 af

--- Week2/ex6/ex6.py
+++ Week2/ex6/ex6.py
@@ -1,6 +1,6 @@
 import sys
 
-knapsack = [1,4,7,10,13,15,28,34,57,59,64,75,85,109,123,137]
+knapsack = [2, 5, 9, 18, 40, 84, 175, 345, 712, 1400, 2840, 5777, 12032, 25323, 50213, 103421]
 
 #get sum of all
 total = 0
@@ -8,7 +8,7 @@ for x in knapsack:
 	total=total+x
 
 #choose n and m
-n = total + 174 # = 995
+n = total + 1075 # = 203471
 m = 64
 print ("n = " + str(n) + ", m = " + str(m))
 
@@ -28,34 +28,37 @@ for x in range(1, n) :
 line = sys.stdin.readline()
 line = line.split()
 
-numbers = []
+#parse input into numbers
+encrypted = []
 for x in range(0,len(line)):
-	numbers.append(int(line[x]))
+	encrypted.append(int(line[x]))
 
-for x in numbers:
-	print (bin(x))
+#perform needed calculations with the inverse
+inverted = []
+for x in encrypted:
+	inverted.append(x*inverse%n)
 
+#use knapsack to decrypt the inverted numbers
+decrypted = []
+for x in inverted:
+	num = x
+	block = 0
+	for y in range(len(knapsack)-1, -1, -1):
+		block = block * 2
+		if num >= knapsack[y]:
+			num = num - knapsack[y]
+			block = block + 1 
+	decrypted.append(block)
+			
+#decrypted[0]        - original
+#decrypted[0] >> 8   - leftmost 8 bits
+#decrypted[0] & 255  - rightmost 8 bits
 
-
-
-'''
-frame = bytearray()
-for x in line:
-	frame.append(int(x))
-
-print (bin(frame[1] & 0b11000000))
-
-q = 0b00110101
-print (q >> 9 & 0b1)
-
-print(bin(q))
-
-
-
-test = 0b001000
-print (bin(test))
-print (test)
-test = test << 8
-print (bin(test))
-print (test)
-'''
+#split the blocks and enter into string
+decyphered = ""
+for x in decrypted:
+	decyphered = decyphered + chr(x >> 8)
+	decyphered = decyphered + chr(x & 255)
+	
+	
+print (decyphered)
